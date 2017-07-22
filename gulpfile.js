4fd2a86a086327dfe4ecf72c8af1fe2cbc820779
Dan Allen <dan.j.allen@gmail.com>
2017-07-21 19:23:00 -0600

    use consolidated syntax in gulpfile [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const path = require('path')
 const gulp = require('gulp')
 
+const config = require('./config')
+
 const buildTheme = require('./tasks/build-theme')
 const buildPreview = require('./tasks/build-preview')
 const buildRelease = require('./tasks/build-release')
@@ -10,18 +12,12 @@ const preview = require('./tasks/preview')
 const release = require('./tasks/release')
 const update = require('./tasks/update')
 
-const config = require('./config')
 try {
   config.validate({ allowed: 'strict' })
 }
 catch (error) {
-  const configErrorMessages = error.message.split('\n')
-  configErrorMessages.forEach((message) => {
-    console.error('Bad config -', message)
-  })
-
-  // 9 - Invalid Argument
-  // https://nodejs.org/api/process.html#process_exit_codes
+  error.message.split('\n').forEach((message) => { console.error('Bad config -', message) })
+  // 9 - Invalid Argument; see https://nodejs.org/api/process.html#process_exit_codes
   process.exit(9)
 }
 
@@ -29,35 +25,31 @@ const src = config.get('source')
 const dest = config.get('destination')
 const destTheme = path.join(dest, config.get('theme_destination'))
 
-gulp.task('build-theme', () => {
-  return buildTheme(src, destTheme)
-})
+gulp.task('build-theme', () =>
+  buildTheme(src, destTheme)
+)
 
-gulp.task('build-preview', ['build-theme'], () => {
-  return buildPreview(src, dest, destTheme)
-})
+gulp.task('build-preview', ['build-theme'], () =>
+  buildPreview(src, dest, destTheme)
+)
 
-gulp.task('preview', ['build-preview'], () => {
-  return preview({ dest, port: config.get('port') }, () => gulp.start('build-preview'))
-})
+gulp.task('preview', ['build-preview'], () =>
+  preview({ dest, port: config.get('port') }, () => gulp.start('build-preview'))
+)
 
-gulp.task('build-release', ['build-theme'], () => {
-  return buildRelease({
-    repo: config.get('repository.name'),
-    dest,
-    destTheme,
-  })
-})
+gulp.task('build-release', ['build-theme'], () =>
+  buildRelease({ repo: config.get('repository.name'), dest, destTheme })
+)
 
-gulp.task('release', ['build-release'], () => {
-  return release({
+gulp.task('release', ['build-release'], () =>
+  release({
     owner: config.get('repository.owner'),
     repo: config.get('repository.name'),
     token: config.get('github_token'),
-    dest,
+    dest
   })
-})
+)
 
-gulp.task('update', () => {
-  return update()
-})
+gulp.task('update', () =>
+  update()
+)
