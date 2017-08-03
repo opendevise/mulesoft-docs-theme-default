46b56d5071c96e818df077ac32ea273d3b3247a0
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-08-03 09:07:18 +0200

    retain the collapsed/expanded state of the menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,32 @@
 !(function () {
   'use strict'
 
-  const $toggleButtons = Array.from(document.querySelectorAll('.nav-tgl'))
+  restoreExpandedSate()
 
-  $toggleButtons.forEach((btn) => {
-
+  qsa('.nav-tgl').forEach((btn) => {
     const li = btn.parentElement
     btn.addEventListener('click', () => {
       const collapsed = (li.dataset.state === 'collapsed' || li.dataset.state == null)
       li.dataset.state = collapsed ? 'expanded' : 'collapsed'
+      saveExpandedState()
     })
   })
+
+  function restoreExpandedSate() {
+    const jsonExpandedLinks = sessionStorage.getItem('expanded-links')
+    const expandedLinks = (jsonExpandedLinks != null) ? JSON.parse(jsonExpandedLinks) : []
+    qsa('.inner-nav .nav-lnk')
+      .filter((link) => expandedLinks.includes(link.href))
+      .forEach((link) => link.parentElement.dataset.state = 'expanded')
+  }
+
+  function saveExpandedState() {
+    const expandedLinks = qsa('.inner-nav .nav-itm[data-state="expanded"] > .nav-lnk')
+      .map((a) => a.href)
+    sessionStorage.setItem('expanded-links', JSON.stringify(expandedLinks))
+  }
+
+  function qsa(selector) {
+    return Array.from(document.querySelectorAll(selector))
+  }
 })()
