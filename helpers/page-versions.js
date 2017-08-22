18406ffa54a4091639166876874c426096656a52
Dan Allen <dan.j.allen@gmail.com>
2017-08-22 15:37:46 -0600

    rename properties on domain version object
    - rename domainTitle to title
    - rename domain to name
    - rename version-label to produce-version-indicator

--- helpers/page-versions.js
+++ helpers/page-versions.js
@@ -2,7 +2,7 @@
 
 // FIXME the UI model should be prepopulated with this collection
 module.exports = (domains, domainName, otherPageVersions) => {
-  const domain = domains.find((candidate) => candidate.domain === domainName), pageVersions = []
+  const domain = domains.find((candidate) => candidate.name === domainName), pageVersions = []
   domain.versions.forEach((domainVersion) => {
     const pageVersion = otherPageVersions.find((candidate) => candidate.string === domainVersion.string)
     pageVersions.push(pageVersion ? pageVersion : Object.assign({ missing: true }, domainVersion))
