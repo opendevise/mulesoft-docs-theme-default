e96b2f530c5b462b0b5214d5be635066595032a4
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-08-25 17:52:58 +0200

    implement tab ui behaviour for navigation panels

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -3,6 +3,13 @@
 
   var navWrapper = document.querySelector('.navigation__wrapper')
 
+  selectPanel('domain')
+  find('.navigation-toolbar [data-panel]').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      selectPanel(btn.dataset.panel)
+    })
+  })
+
   document.querySelector('.navigation-open').addEventListener('click', function () {
     document.body.setAttribute('data-overlay', 'navigation')
     navWrapper.dataset.state = 'open'
@@ -12,4 +19,14 @@
     document.body.removeAttribute('data-overlay')
     navWrapper.dataset.state = null
   })
+
+  function selectPanel(panelName) {
+    find('.navigation [data-panel]').forEach(function (element) {
+      element.classList.toggle('navigation--currentPanel', element.dataset.panel === panelName)
+    })
+  }
+
+  function find(selector) {
+    return [].slice.call(navWrapper.querySelectorAll(selector))
+  }
 })()
