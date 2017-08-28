12135e14f3c32c4840a665737198bfe8cbf9f2b8
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2017-08-28 19:20:28 +0200

    implement more global state manager for navigation panels

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,32 +1,130 @@
 !(function () {
   'use strict'
 
-  var navWrapper = document.querySelector('.navigation__wrapper')
+  var navWrapper = document.querySelector('.navigation__wrapper'),
+    navPanels = document.querySelector('.navigation-panels'),
+    currentDomain = document.querySelector('.nav-tree').dataset.domain,
+    currentVersion = document.querySelector('.nav-tree').dataset.version,
+    state = getState()
 
-  selectPanel('domain')
+  // navigation panels state
+  selectPanel(state.panel)
   find('.navigation-toolbar [data-panel]').forEach(function (btn) {
     btn.addEventListener('click', function () {
       selectPanel(btn.dataset.panel)
     })
   })
 
-  document.querySelector('.navigation-open').addEventListener('click', function () {
-    document.body.setAttribute('data-overlay', 'navigation')
-    navWrapper.dataset.state = 'open'
-  })
-
-  document.querySelector('.navigation-close').addEventListener('click', function () {
-    document.body.removeAttribute('data-overlay')
-    navWrapper.dataset.state = null
-  })
-
   function selectPanel(panelName) {
     find('.navigation [data-panel]').forEach(function (element) {
       element.classList.toggle('navigation--currentPanel', element.dataset.panel === panelName)
     })
+    if (panelName !== state.panel) {
+      navPanels.scrollTop = 0
+    }
+    state.panel = panelName
+    saveState()
   }
 
-  function find(selector) {
-    return [].slice.call(navWrapper.querySelectorAll(selector))
+  // navigation toggle for mobile/tablet view
+  document.querySelector('.navigation-open').addEventListener('click', openNavigation)
+  document.querySelector('.navigation-close').addEventListener('click', closeNavigation)
+  navWrapper.addEventListener('click', function (e) {
+    e.stopPropagation()
+  })
+  window.addEventListener('click', closeNavigation)
+
+  function openNavigation(e) {
+    e.stopPropagation()
+    document.body.setAttribute('data-overlay', 'navigation')
+    navWrapper.dataset.state = 'open'
+  }
+
+  function closeNavigation() {
+    document.body.removeAttribute('data-overlay')
+    navWrapper.dataset.state = null
+  }
+
+  // scroll position of the current panel
+  navPanels.scrollTop = state.scroll
+  navPanels.addEventListener('scroll', function () {
+    state.scroll = parseInt(navPanels.scrollTop)
+    saveState()
+  })
+
+  // navigation tree items
+  find('.nav-tree').forEach(function (navTree) {
+    var panel = navTree.parentElement.dataset.panel
+    find('.nav-itm', navTree).forEach(function (item, idx) {
+      item.setAttribute('data-id', [panel, item.dataset.depth, idx].join('-'))
+    })
+  })
+  find('.nav-ctl').forEach(function (btn) {
+    var li = btn.parentElement
+    btn.addEventListener('click', function () {
+      li.setAttribute('data-state', (li.dataset.state === 'collapsed' || !li.dataset.state) ? 'expanded' : 'collapsed')
+      state.expandedItems = getExpandedItems()
+      saveState()
+    })
+  })
+  if (state.expandedItems == null) {
+    state.expandedItems = getExpandedItems()
+    saveState()
+  }
+  if (state.domain !== currentDomain || state.version !== currentVersion) {
+    var currentPageItem = document.querySelector('.nav-itm--currentPage')
+    if (currentPageItem) {
+      scrollItemIntoView(currentPageItem, navPanels)
+    }
+    state.expandedItems = state.expandedItems.filter(function (item) {
+      return item.match(/^aspect-/)
+    })
+  }
+
+  state.expandedItems.forEach(function (itemId) {
+    var item = document.querySelector('.nav-itm[data-id="' + itemId + '"]')
+    if (item != null) {
+      item.setAttribute('data-state', 'expanded')
+    }
+  })
+  saveState()
+
+  function getExpandedItems() {
+    return find('.nav-tree .nav-itm[data-state="expanded"]').map(function (item) {
+      return item.dataset.id
+    })
+  }
+
+  // state management
+  function getState() {
+    if (sessionStorage.getItem('nav-state') != null) {
+      return JSON.parse(sessionStorage.getItem('nav-state'))
+    }
+    else {
+      return {
+        panel: 'domain',
+        scroll: 0,
+        domain: currentDomain,
+        version: currentVersion,
+        expandedItems: null,
+      }
+    }
+  }
+
+  function saveState() {
+    sessionStorage.setItem('nav-state', JSON.stringify(state))
+  }
+
+  // tries to get item as close to the top of the view as possible
+  function scrollItemIntoView(el, parent) {
+    var amountToScroll = el.offsetTop - parent.offsetTop
+    if (amountToScroll > 0) {
+      parent.scrollTop = amountToScroll
+    }
+  }
+
+  function find(selector, from) {
+    from = from || document
+    return [].slice.call(from.querySelectorAll(selector))
   }
 })()
